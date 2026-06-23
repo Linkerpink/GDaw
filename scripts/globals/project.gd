fcec4de34130e9dd186a032d7e47ce2b26a81959
Linkerpink <noahvanuunen2007@gmail.com>
2026-06-23 08:51:56 +0200

    bpm tapper added

--- scripts/globals/project.gd
+++ scripts/globals/project.gd
@@ -3,6 +3,15 @@ extends Node
 var file_name : String = "project"
 var bpm : float = 120
 
+var bpm_manager : Control
+var bpm_spin_box : SpinBox
+
+
+func _ready() -> void:
+	bpm_manager = get_tree().get_first_node_in_group("bpm_manager")
+	bpm_spin_box = bpm_manager.find_child("BPMSpinBox")
+
 
 func set_bpm(_bpm : float):
 	bpm = _bpm
+	bpm_spin_box.value = bpm
--- scripts/windows/bpm_tapper.gd
+++ scripts/windows/bpm_tapper.gd
@@ -1,18 +1,53 @@
 extends Control
 
 @onready var bpm_label : Label = %BPMLabel
-var bpm_before_tapping : float
+@onready var tap_button : Button = $VBoxContainer/TapButton
+
+var time_to_stop: float = 2.0
+var tap_times: Array[float] = []
+var bpm : float = 0.0
+var bpm_before_tapping : float = 0.0
 
 
 func _ready() -> void:
-	bpm_before_tapping = 120
-	bpm_label.text = str(bpm_before_tapping)
+	bpm_before_tapping = Project.bpm
+	bpm_label.text = "Tap to calculate BPM"
+	
+	tap_button.grab_focus()
 
 
 func _tap_bpm() -> void:
-	bpm_label.show()
-	print("tap bpm")
+	var current_time = Time.get_unix_time_from_system()
+	
+	if tap_times.size() > 0 and current_time - tap_times[-1] > time_to_stop:
+		tap_times.clear()
+	
+	tap_times.append(current_time)
+	_calculate_bpm()
 
 
 func _on_reset_button_pressed() -> void:
-	print("reset bpm")
+	bpm = bpm_before_tapping
+	Project.set_bpm(bpm_before_tapping)
+	bpm_label.text = "Tap to calculate BPM"
+	
+	tap_times.clear()
+
+
+func _calculate_bpm() -> void:
+	if tap_times.size() < 2:
+		bpm_label.text = "Calculating BPM"
+	
+	var time_diffs: Array[float] = []
+	for i in range(1, tap_times.size()):
+		time_diffs.append(tap_times[i] - tap_times[i-1])
+	
+	var average_diff: float = 0.0
+	for diff in time_diffs:
+		average_diff += diff
+	average_diff /= time_diffs.size()
+	
+	if average_diff > 0:
+		bpm = 60.0 / average_diff
+		bpm_label.text = str(roundf(bpm))
+		Project.set_bpm(bpm)
